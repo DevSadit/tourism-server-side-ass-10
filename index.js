1b4e0722cae1342df6edcd6357481318e6c25c5a
DevSadit <de.sadit@gmail.com>
2024-04-28 04:59:48 +0600

    creat read and delete operation completed

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middelware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irm8dks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,9 +24,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const spotCollection = client.db(`spotDB`).collection(`spot`);
 
+    app.get(`/spotDetails`, async (req, res) => {
+      const cursor = spotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    
+    app.post(`/spotDetails`, async (req, res) => {
+      const newSpot = req.body;
+      console.log(newSpot);
+      const result = await spotCollection.insertOne(newSpot);
+      res.send(result);
+    });
+
+    app.delete(`/spotDetails/:id`, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await spotCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -37,18 +58,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-// middelware
-app.use(cors());
-app.use(express.json());
-
-
-app.get("/", (req, res)=>{
-    res.send(`Server is Running`)
-})
-app.listen(port, ()=>{
-    console.log(`server is running on port : ${port}`);
-})
+app.get("/", (req, res) => {
+  res.send(`Server is Running`);
+});
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`);
+});
